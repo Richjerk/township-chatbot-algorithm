fa7e7dcf022a380bfbafac804be9e5c46de9f2ec
Richjerk <49521848+Richjerk@users.noreply.github.com>
2023-11-07 09:10:57 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,47 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import { AppProps } from 'next/app';
+import { lazy } from 'react';
 
-import { AppProps } from 'next/app'
-import { lazy } from 'react'
+// Import your necessary components and providers here.
+// Make sure you import the required components and providers related to the chatbot functionality.
 
 export interface SharedPageProps {
-  draftMode: boolean
-  token: string
+  draftMode: boolean;
+  token: string;
 }
 
-const PreviewProvider = lazy(() => import('components/PreviewProvider'))
+// Lazy-load the necessary components here.
+// You may need to import components specific to your chatbot features.
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<SharedPageProps>) {
-  const { draftMode, token } = pageProps
+  const { draftMode, token } = pageProps;
+
   return (
+    // You can include your chatbot components and providers here.
+    // Depending on the architecture and structure of your chatbot, you may have different components and providers to integrate.
+
     <>
       {draftMode ? (
-        <PreviewProvider token={token}>
-          <Component {...pageProps} />
-        </PreviewProvider>
+        // Render components and providers for draft mode.
+        // You may have specific components and logic for draft mode.
+        // Include them here.
+        // Example: <DraftModeProvider token={token}>
+        //              <Component {...pageProps} />
+        //           </DraftModeProvider>
+      ) : (
+        // Render components and providers for regular mode.
+        // Include the components and logic for regular mode here.
+        // Example: <RegularModeProvider>
+        //              <Component {...pageProps} />
+        //           </RegularModeProvider>
+      )}
+    </>
+  );
+}
+vider>
       ) : (
         <Component {...pageProps} />
       )}
